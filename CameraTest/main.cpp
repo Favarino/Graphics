54a2916bfc038d2d740f4d98c837ecd06810d956
Favarino <n.long1520@gmail.com>
2016-09-08 15:21:28 -0700

    basic lighting effect

--- CameraTest/main.cpp
+++ CameraTest/main.cpp
@@ -22,21 +22,31 @@ int main()
 	input.init(window);
 	time.init();
 
-	glm::mat4 view = glm::lookAt(glm::vec3(5.f, 5.f, 5.f),
-								 glm::vec3(0.f, 0.f, 0.f), 
-								 glm::vec3(0.f, 1.f, 0.f));
+	glm::mat4 view = glm::lookAt(glm::vec3(5.f, 5.f, 5.f), //eye
+								 glm::vec3(0.f, 0.f, 0.f), //center
+								 glm::vec3(0.f, 1.f, 0.f));//up
 
 	glm::mat4 proj = glm::perspective(45.f, 16/9.f, 1.f, 100.f);
 	glm::mat4 model;
 
-	Geometry geo = loadOBJ("../res/models/cube.obj");
+	Geometry cube = loadOBJ("../res/models/cube.obj");
+
+	Geometry sphere = loadOBJ("../res/models/sphere.obj");
 
 	Shader shader = loadShader("../res/shaders/phongVert.txt", 
 							   "../res/shaders/phongFrag.txt");
 
+	
+	float timer = 0;
 	while (window.step())
 	{
-		drawPhong(shader, geo, glm::value_ptr(model), glm::value_ptr(view), glm::value_ptr(proj));
+		timer += 0.016;
+
+		glm::mat4 modelC = glm::rotate(timer, glm::normalize(glm::vec3(0, 1, 1)));
+		glm::mat4 modelS = glm::translate(glm::vec3(0, cos(timer) * 6, 0));
+
+		drawPhong(shader, cube, glm::value_ptr(modelC), glm::value_ptr(view), glm::value_ptr(proj));
+		drawPhong(shader, sphere, glm::value_ptr(modelS), glm::value_ptr(view), glm::value_ptr(proj));
 	}
 
 	gallery.term();
